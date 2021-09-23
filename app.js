6f2ff01946128b8622bf4b4906084bb9a3e5ef9c
Souvik Pal <spal62588@gmail.com>
2021-09-23 13:04:42 +0530

    Mailing System Done

---
+++ app.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.USER_EMAIL,
+        pass: process.env.USER_PASSWORD
+    }
+});
+
+async function sendMail() {
+	try{
+		let info = await transporter.sendMail({
+    		from: "certifier.mailer@gmail.com",
+    		to: "berasumit956@gmail.com",
+    		subject: "Mailing System using NodeJS",
+    		text: "Hello, My Friend",
+    		attachments: [
+		        {
+		            path: __dirname + '/Files/certificate.pdf'
+		        }
+	    	]
+  		});
+  		console.log("Mail Sent: %s", info.messageId);
+	}
+	catch(err){
+		console.log(`Error : ${err}`);
+	}
+}
+
+sendMail();
